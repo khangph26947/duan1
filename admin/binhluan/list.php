7f3aa7b9fe10fdd058d1ef465baf6b4079214ee9
manhledinhph23984 <109653032+manhledinhph23984@users.noreply.github.com>
2023-04-06 08:26:06 +0700

    Sửa css list.php binhluan

--- admin/binhluan/list.php
+++ admin/binhluan/list.php
@@ -1,147 +1,50 @@
-<?php
-session_start();
-include "../../model/pdo.php";
-include "../../model/binhluan.php";
-
-$idpro = $_REQUEST['idpro'];
-$dsbl = loadall_binhluan($idpro);
-
-function push_notification($type, $msgs)
-{
-    if (!isset($_SESSION["notification"])) $_SESSION["notification"] = [];
-    $data = [];
-    $data["type"] = $type;
-    $data["msgs"] = $msgs;
-    $_SESSION["notification"][] = $data;
-}
-
-function get_notification()
-{
-    if (!isset($_SESSION["notification"])) $_SESSION["notification"] = [];
-    $notification = $_SESSION["notification"];
-    unset($_SESSION["notification"]);
-    return $notification;
-}
-?>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="../css/style.css">
-    <link rel="stylesheet" href="../css/bootstrap.css">
-    <link rel="stylesheet" href="../css/style2.css">
-    <style>
-        .boxbl {
-            border: 1px solid #ccc;
-            background-color: #fff;
-            margin-top: 20px;
-        }
-
-        .bl h3 {
-            border-bottom: 1px solid #ccc;
-            padding: 12px 5px;
-            background-color: #f2f2f2;
-        }
-
-        .binhluan {
-            margin-top: -20px;
-            margin-left: 30px;
-        }
-
-        .binhluan table {
-            width: 100%;
-        }
-
-        .binhluan table td:nth-child(1) {
-            width: 35%;
-        }
-
-        .binhluan table td:nth-child(2) {
-            width: 35%
-        }
-
-        .binhluan table td:nth-child(3) {
-            width: 30%;
-        }
-
-        .timkiem {
-            border-top: 1px solid #ccc;
-            background-color: #f2f2f2;
-            padding: 8px 5px;
-        }
-    </style>
 
 
-</head>
-
-<body>
-    <div class="boxbl">
-        <div class="left-side bl">
-            <h3 class="sear-head ">Bình luận</h3>
-            <div class="w3layouts-box-list binhluan">
+        <div class="row1 form__list">
+      <div class="from">
+          <h2>Quản Lý Tài khoản</h2>
+      </div>
+    <div class="from_list2">
+                <div class="from_table">
+                    <h2>Danh sách Tài khoản</h2>
+                    <p>Danh sách các tài khoản có trên hệ thống</p>
+                </div>
+            <div class="from_table-content">
                 <table>
+                    <tr>
+                        <th class="bluan3"></th>
+                        <th class="bluan3">Mã tài khoản</th>
+                        <th class="bluan3">Tên Đăng nhập</th>
+                        <th class="bluan3">Mật khẩu </th>
+                        <th class="bluan3">Email</th>
+                        <th class="bluan3">Địa chỉ</th>
+                        <th class="bluan3">Vai trò</th>
+                        <th class="bluan3">Hành động</th>
+                    </tr>
                     <?php
+                    foreach ($listtaikhoan as $taikhoan) {
+                        extract($taikhoan);
+                        $suatk="index.php?act=suatk&id=".$id;
+                        $xoatk="index.php?act=xoatk&id=".$id;
+                       echo'
+                       <tr>
+                       <td class="bluan1"><input type="checkbox"></td>
+                       <td class="bluan1">'.$id.'</td>
+                       <td class="bluan1">'.$user.'</td>
+                       <td class="bluan1">'.$pass.'</td>
+                       <td class="bluan1">'.$email.'</td>
+                       <td class="bluan1">'.$address.'</td>
+                   
+                       <td class="bluan1">'.$role.'</td>
+                       <td class="bluan1"> 
+                       <a href="'. $suatk.'"><input class="mau1" type="button" value="sửa"></a>
 
-                    foreach ($dsbl as $bl) {
-                        extract($bl);
-                        echo '<tr><td><span class="span">' . $noidung . '</span></td>';
-                        echo '<td><span class="span">' . $name . '</span></td>';
-                        echo '<td><span class="span">' . $ngaybinhluan . '</span></td></tr>';
+                           <a href="'. $xoatk.'"><input class="mau2" type="button" value="xóa"></a>
+                       
+                       </td>
+                   </tr>';
                     }
                     ?>
                 </table>
-                <!-- echo "Nội dung bình luận ở đây: ".$idpro; -->
-
             </div>
-        </div>
-
-        <div class="search-bar w3layouts-newsletter timkiem">
-            <form action="<?= $_SERVER['PHP_SELF']; ?>" method="post" class="d-flex">
-                <input type="hidden" name="idpro" value="<?= $idpro ?>">
-
-                <input type="text" name="noidung">
-                <input type="submit" name="guibinhluan" value="Gửi bình luận">
-            </form>
-            <div>
-                <?php $notifications = get_notification(); ?>
-                <?php foreach ($notifications as $notification) : ?>
-                    <div class="m-[10px] text-[red]">
-                        <?php foreach ($notification['msgs'] as $msg) : ?>
-                            <div class="<?php echo $notification['type'] ?>"><?php echo $msg ?></div>
-                        <?php endforeach; ?>
-                    </div>
-                <?php endforeach; ?>
-            </div>
-        </div>
-
-        <?php
-        if (isset($_POST['guibinhluan']) && ($_POST['guibinhluan'])) {
-            if (isset($_SESSION['user'])) {
-                $noidung = $_POST['noidung'];
-                $idpro = $_POST['idpro'];
-                $iduser = $_SESSION['user']['id'];
-                $name = $_SESSION['user']['user'];
-                $ngaybinhluan = date('h:i:sa d/m/Y');
-                insert_binhluan($name, $noidung, $iduser, $idpro, $ngaybinhluan);
-                unset($_SESSION['errcmt']);
-                header("location: " . $_SERVER['HTTP_REFERER']);
-            } else {
-                push_notification('errors', [
-                    'name' => 'Bạn phải đăng nhập để bình luận'
-                ]);
-                header("location: " . $_SERVER['HTTP_REFERER']);
-            }
-        }
-
-        ?>
     </div>
-
-
-
-</body>
-
-</html>
